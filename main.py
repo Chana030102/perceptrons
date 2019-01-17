22ca4f877262c294e667e07cbdba928ca9738cf1
Chana030102 <chanaar@pdx.edu>
2019-01-17 02:34:01 -0800

    Remove loop for learning rates

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import myperceptron
 
 EPOCH_MAX  = 2
 INPUT_MAX  = 255
-learning_rate = [0.01, 0.1, 1]
+rate = 1
 
 TRAIN_FILE = "../mnist_train.csv"
 TEST_FILE  = "../mnist_test.csv"
@@ -48,84 +48,83 @@ test_data = numpy.divide(test_data, INPUT_MAX)
 
 input_size = len(train_data[0]) # how many inputs are there
 
-for rate in learning_rate:
-    # new network of perceptrons
-    net = [myperceptron.Perceptron(input_size) for i in range(0,10)]
+# new network of perceptrons
+net = [myperceptron.Perceptron(input_size) for i in range(0,10)]
 
-    # Epoch 0 accuracy test for train data
+# Epoch 0 accuracy test for train data
+for i in range(0,len(train_data)):
+    for j in range(0,10):
+        output[j] = net[j].evaluate(train_data[i])
+    
+    if(train_target[i] == output.index(max(output))):
+        accuracy['train_c'][0]+=1
+    else:
+        accuracy['train_i'][0]+=1
+    
+# Epoch 0 accuracy test for test data
+for i in range(0,len(test_data)):
+    for j in range(0,10):
+        output[j] = net[j].evaluate(test_data[i])
+
+    if(output.index(max(output)) == test_target[i]):
+        accuracy['test_c'][0]+=1
+    else:
+        accuracy['test_i'][0]+=1
+
+# Start training and record accuracy after each epoch
+for e in range(1,EPOCH_MAX+1):
+
+    # Loop through each row of training data
+    for x in range(0,len(train_data)):
+        # Loop through each perceptron
+        for y in range(0,10):
+            output[y] = net[y].evaluate(train_data[x])
+
+        # Evaluate outputs. Greatest value is prediction and equals 1. Others are 0
+        prediction = output.index(max(output))
+        for i in range(0,10):
+            if(i==prediction):
+                output[i] = 1
+            else:
+                output[i] = 0
+        
+        # Update weights based off prediction
+        for y in range(0,10):
+            if(y==train_target[x]):
+                net[y].updateWeights(1,output[y],train_data[x],rate)
+            else:
+                net[y].updateWeights(0,output[y],train_data[x],rate)
+
+    # Evaluate and test accuracy at each epoch with training data
     for i in range(0,len(train_data)):
         for j in range(0,10):
             output[j] = net[j].evaluate(train_data[i])
         
-        if(train_target[i] == output.index(max(output))):
-            accuracy['train_c'][0]+=1
+        if(output.index(max(output)) == train_target[i]):
+            accuracy['train_c'][e]+=1
         else:
-            accuracy['train_i'][0]+=1
+            accuracy['train_i'][e]+=1
         
-    # Epoch 0 accuracy test for test data
+    # Evaluate and test accuracy at each epoch with test data
     for i in range(0,len(test_data)):
         for j in range(0,10):
             output[j] = net[j].evaluate(test_data[i])
-
+        
         if(output.index(max(output)) == test_target[i]):
-            accuracy['test_c'][0]+=1
+            accuracy['test_c'][e]+=1
         else:
-            accuracy['test_i'][0]+=1
+            accuracy['test_i'][e]+=1
 
-    # Start training and record accuracy after each epoch
-    for e in range(1,EPOCH_MAX+1):
-
-        # Loop through each row of training data
-        for x in range(0,len(train_data)):
-            # Loop through each perceptron
-            for y in range(0,10):
-                output[y] = net[y].evaluate(train_data[x])
-
-            # Evaluate outputs. Greatest value is prediction and equals 1. Others are 0
-            prediction = output.index(max(output))
-            for i in range(0,10):
-                if(i==prediction):
-                    output[i] = 1
-                else:
-                    output[i] = 0
-            
-            # Update weights based off prediction
-            for y in range(0,10):
-                if(y==train_target[x]):
-                    net[y].updateWeights(1,output[y],train_data[x],rate)
-                else:
-                    net[y].updateWeights(0,output[y],train_data[x],rate)
-
-        # Evaluate and test accuracy at each epoch with training data
-        for i in range(0,len(train_data)):
-            for j in range(0,10):
-                output[j] = net[j].evaluate(train_data[i])
-            
-            if(output.index(max(output)) == train_target[i]):
-                accuracy['train_c'][e]+=1
-            else:
-                accuracy['train_i'][e]+=1
-            
-        # Evaluate and test accuracy at each epoch with test data
-        for i in range(0,len(test_data)):
-            for j in range(0,10):
-                output[j] = net[j].evaluate(test_data[i])
-            
-            if(output.index(max(output)) == test_target[i]):
-                accuracy['test_c'][e]+=1
-            else:
-                accuracy['test_i'][e]+=1
+# Finished training for this learning rate. Construct confusion matrix
+for i in range(0,len(test_data)):
+    for j in range(0,10):
+        output[j] = net[j].evaluate(test_data[i])
     
-    # Finished training for this learning rate. Construct confusion matrix
-    for i in range(0,len(test_data)):
-        for j in range(0,10):
-            output[j] = net[j].evaluate(test_data[i])
-        
-        # Confusion matrix --> confu[actual][prediction]
-        confu[test_target[i]][output.index(max(output))]+=1
-    
-    # Output accuracy and confusion matrix to CSV file
-    accuracy_title = 'accuracy_rate='+str(rate)+'.csv'
-    cmatrix_title = 'confusionmatrix_rate='+str(rate)+'.csv'
-    accuracy.to_csv(accuracy_title)
-    confu.to_csv(cmatrix_title)
+    # Confusion matrix --> confu[actual][prediction]
+    confu[test_target[i]][output.index(max(output))]+=1
+
+# Output accuracy and confusion matrix to CSV file
+accuracy_title = 'accuracy_rate='+str(rate)+'.csv'
+cmatrix_title = 'confusionmatrix_rate='+str(rate)+'.csv'
+accuracy.to_csv(accuracy_title)
+confu.to_csv(cmatrix_title)
